c621982a3e04438b364bfc4c30e04fc1d0d32f72
Dmitry <irlitsa.d.p@gmail.com>
2021-03-04 14:37:21 +0300

    update homework_6.1

--- homework_6.1/src/BankAccount.java
+++ homework_6.1/src/BankAccount.java
@@ -1,9 +1,9 @@
 public class BankAccount {
-  private double amount;
+    private double amount;
 
-  public double getAmount() {
-    return amount;
-  }
+    public double getAmount() {
+        return amount;
+    }
 
     public void put(double amountToPut) {
         if (amountToPut > 0) {
@@ -11,18 +11,18 @@ public class BankAccount {
         }
     }
 
-  public void take(double amountToTake) {
-    if (amountToTake > 0 && amountToTake <= amount) amount -= amountToTake;
-  }
-
-  public boolean send(BankAccount receiver, double amount) {
-    double amountSenderBeforeSend = this.getAmount();
-    this.take(amount);
-    if (this.getAmount() != amountSenderBeforeSend) {
-      receiver.put(amount);
-      return true;
-    } else {
-      return false;
+    public void take(double amountToTake) {
+        if (amountToTake > 0 && amountToTake <= amount) amount -= amountToTake;
     }
-  }
+
+    public boolean send(BankAccount receiver, double amount) {
+        double amountSenderBeforeSend = this.getAmount();
+        this.take(amount);
+        if (this.getAmount() != amountSenderBeforeSend) {
+            receiver.put(amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
