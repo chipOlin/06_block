d88ca9d010eab48001f491e9d7c409b55a17706e
Dmitry <irlitsa.d.p@gmail.com>
2021-03-04 14:34:47 +0300

    update homework_6.1

--- homework_6.1/src/BankAccount.java
+++ homework_6.1/src/BankAccount.java
@@ -1,17 +1,17 @@
 public class BankAccount {
-  private double amount;
+    private double amount;
 
-  public double getAmount() {
-    return amount;
-  }
-
-  public void put(double amountToPut) {
-    if(amountToPut > 0) {
-      amount += amountToPut;
+    public double getAmount() {
+        return amount;
     }
-  }
 
-  public void take(double amountToTake) {
-    if (amountToTake <= amount) amount -= amountToTake;
-  }
+    public void put(double amountToPut) {
+        if (amountToPut > 0) {
+            amount += amountToPut;
+        }
+    }
+
+    public void take(double amountToTake) {
+        if (amountToTake <= amount) amount -= amountToTake;
+    }
 }
